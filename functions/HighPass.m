4e69c4e93137ff80404e822d40e7c2a319017dd2
Andres de Vicente <andres.devicente@fmi.ch>
2026-02-17 12:20:35 +0100

    Keep only RIPPLES and README

--- functions/HighPass.m
+++
@@ -1,4 +0,0 @@
-function singnalFiltered = HighPass(signal,fech, fHigh,order)
-
-[num,den]=butter(order,fHigh/(fech/2),'high') ;
-singnalFiltered = filtfilt(num,den,signal);
--- functions/LowPass.m
+++
@@ -1,4 +0,0 @@
-function singnalFiltered = LowPass(signal,fech, fLow,order)
-
-[num,den]=butter(order,fLow/(fech/2),'low') ;
-singnalFiltered = filtfilt(num,den,signal);
